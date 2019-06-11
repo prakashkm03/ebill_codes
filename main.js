b6efe4f315e7c0a63ee2f0c783ecab88d307711f
Prakash Kumar <39598067+prakashkm03@users.noreply.github.com>
2019-06-11 23:47:16 +0530

    for hosting

---
+++ main.js
@@ -0,0 +1,14 @@
+var Data = {
+  'FRUITS': 1800,
+  'GROCERIES': 3600
+};
+
+var Options = {
+  'height': 200,
+  'width': 10,
+  'fixPadding': 18,
+  'barFont': [0, 10, "thin"],
+  'labelFont': [0, 8, 0]
+};
+
+graphite(Data, Options, chart);
